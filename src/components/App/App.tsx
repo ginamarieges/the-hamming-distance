ca960fa92fcf2faaa5bea88a49c30e76c4f17314
Gina Marieges <gina.marieges@gmail.com>
2023-07-22 10:46:26 +0200

    Modify App component adding Form in it

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,15 @@
+import Form from "../Form/Form";
+
 const App = (): JSX.Element => {
-  return <p>Hello world!</p>;
+  return (
+    <>
+      <h1>The Hamming distance</h1>
+      <h2>
+        In this app you can calculate the Hamming distance between two words!
+      </h2>
+      <Form />
+    </>
+  );
 };
 
 export default App;
