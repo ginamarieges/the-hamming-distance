7c22b2664ccea7d6c724db033fbe31f0182f828d
Gina Marieges <gina.marieges@gmail.com>
2023-07-22 10:00:03 +0200

    Add GlobalStyle with css resets

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body,
+h1,
+h2,
+h3,
+h4 {
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+button,
+input,
+textarea,
+select {
+  font-family: inherit;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+button {
+  cursor: pointer;
+  border: none;
+  padding: 0;
+}
+
+img {
+  max-width: 100%;
+}
+
+`;
+
+export default GlobalStyle;
